915f8094cc28e92f69c4b18a696cc2793de7f1c1
xiaozi0825 <tetty60213@gmail.com>
2016-06-23 13:45:32 +0800

    first commit

---
+++ aspnetFinalTermExam/Models/Employees.cs
@@ -0,0 +1,78 @@
+namespace aspnetFinalTermExam.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("HR.Employees")]
+    public partial class Employees
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Employees()
+        {
+            Employees1 = new HashSet<Employees>();
+            Orders = new HashSet<Orders>();
+        }
+
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string TitleOfCourtesy { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string City { get; set; }
+
+        [StringLength(15)]
+        public string Region { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string Country { get; set; }
+
+        [Required]
+        [StringLength(24)]
+        public string Phone { get; set; }
+
+        public int? ManagerID { get; set; }
+
+        [StringLength(2)]
+        public string Gender { get; set; }
+
+        public int? MonthlyPayment { get; set; }
+
+        public int? YearlyPayment { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Employees> Employees1 { get; set; }
+
+        public virtual Employees Employees2 { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
---
+++ aspnetFinalTermExam/Models/Nums.cs
@@ -0,0 +1,15 @@
+namespace aspnetFinalTermExam.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Nums
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int n { get; set; }
+    }
+}
